88207b64727eff8619df17106f7b85c872b1aba0
Rares Moldovan <rares.moldovan@garmin.com>
2020-03-19 13:53:54 +0200

    Spring integration

--- src/main/java/ubb/tourism/Application.java
+++ src/main/java/ubb/tourism/Application.java
@@ -1,47 +1,24 @@
 package ubb.tourism;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import ubb.tourism.data.access.entity.Flight;
-import ubb.tourism.data.access.entity.Ticket;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ubb.tourism.data.access.repository.FlightRepository;
 import ubb.tourism.data.access.repository.UserRepository;
 import ubb.tourism.data.access.repository.impl.FlightRepositoryImpl;
 import ubb.tourism.data.access.repository.impl.TicketRepositoryImpl;
 import ubb.tourism.data.access.repository.impl.UserRepositoryImpl;
-import ubb.tourism.data.access.utils.JdbcUtils;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Properties;
 
 public class Application {
 
-    public static final Logger LOGGER = LogManager.getLogger();
-
     public static void main(String[] args) {
-        testSize();
-    }
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring-context.xml");
 
-    private static void testSize() {
-        Properties properties = getProperties();
-        JdbcUtils jdbcUtils = new JdbcUtils(properties);
-        UserRepository userRepository = new UserRepositoryImpl(jdbcUtils);
-        FlightRepository flightRepository = new FlightRepositoryImpl(jdbcUtils);
-        List<Flight> flightList = (List<Flight>) flightRepository.findAll();
-        TicketRepositoryImpl ticketRepository = new TicketRepositoryImpl(jdbcUtils);
-        List<Ticket> ticketList = (List<Ticket>) ticketRepository.findAll();
-    }
+        UserRepository userRepository = applicationContext.getBean(UserRepositoryImpl.class);
+        TicketRepositoryImpl ticketRepository = applicationContext.getBean(TicketRepositoryImpl.class);
+        FlightRepository flightRepository = applicationContext.getBean(FlightRepositoryImpl.class);
 
-    private static Properties getProperties() {
-        ClassLoader classLoader = Application.class.getClassLoader();
-        Properties properties = new Properties();
-        try {
-            properties.load(classLoader.getResourceAsStream("config/mysql-config.properties"));
-        } catch (IOException e) {
-            LOGGER.error("Error loading mysql configuration file", e);
-        }
-        LOGGER.info("MySQL properties have been loaded successfully");
-        return properties;
+        userRepository.findAll().forEach(System.out::println);
+        ticketRepository.findAll().forEach(System.out::println);
+        flightRepository.findAll().forEach(System.out::println);
     }
 }
--- src/main/java/ubb/tourism/data/access/utils/JdbcUtils.java
+++ src/main/java/ubb/tourism/data/access/utils/JdbcUtils.java
@@ -1,5 +1,10 @@
 package ubb.tourism.data.access.utils;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import ubb.tourism.Application;
+
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,11 +12,20 @@ import java.util.Properties;
 
 public class JdbcUtils {
 
+    private static final Logger LOGGER = LogManager.getLogger(JdbcUtils.class);
+
     private Connection connection = null;
     private Properties properties;
 
+    private String propertiesFileName;
+
     public JdbcUtils(Properties properties) {
         this.properties = properties;
+    }
+
+    public void setPropertiesFileName(String propertiesFileName) {
+        this.propertiesFileName = propertiesFileName;
+        loadProperties();
     }
 
     public Connection getConnection() {
@@ -36,5 +50,15 @@ public class JdbcUtils {
             e.printStackTrace();
         }
         return null;
+    }
+
+    private void loadProperties() {
+        ClassLoader classLoader = Application.class.getClassLoader();
+        try {
+            properties.load(classLoader.getResourceAsStream(propertiesFileName));
+        } catch (IOException e) {
+            LOGGER.error("Error loading mysql configuration file", e);
+        }
+        LOGGER.info("MySQL properties have been loaded successfully");
     }
 }
